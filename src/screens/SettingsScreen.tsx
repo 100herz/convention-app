1d210073253a64a32ce395441403e78fc055690f
Alexander Böhm <mheob@users.noreply.github.com>
2020-12-03 16:23:13 +0100

    build: add release workflow and build settings (rebuild) (#50)
    This is a rebuild to fix a release workflow issue!
    
    * refactor(api): improve the api parameter call
    * build(app): add build configuration
    * test: fix tests after using wallaby and quokka
    * ci(sematic-release): add the release workflow
    * ci(semantic-release): fix the path of the config file
    * ci(sematic-release): add missing node dependencies
    * ci(sematic-release): add missing name property in package.json
    * ci(sematic-release): switch workflow cycjimmy/semantic-release-action
      This is needed to fix an issue causes of the protected branch.

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { ScrollView, StyleSheet, View } from 'react-native'
 
-import SettingsList from '@components/Settings/SettingsList'
+// TODO: Reactivate this setting after adding the push notifications
+// import SettingsList from '@components/Settings/SettingsList'
 import LegalList from '@components/Settings/LegalList'
 import SocialList from '@components/Settings/SocialList'
 import { socialMediaChannels } from '@data/social'
@@ -20,7 +21,7 @@ const SettingsScreen: React.FC = () => {
   return (
     <View style={styles.container}>
       <ScrollView>
-        <SettingsList data={settingArray} />
+        {/* <SettingsList data={settingArray} /> */}
         <LegalList data={legalScreens} />
         <SocialList data={socialMediaChannels} />
       </ScrollView>
